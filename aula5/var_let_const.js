2de0cecc601ed7327a0c56cac91436f47b01d90c
Fellipe Valladares <120605136+FellipeV540@users.noreply.github.com>
2024-03-25 10:31:21 -0300

    Create var_let_const.js

---
+++ aula5/var_let_const.js
@@ -0,0 +1,12 @@
+const preco = 30
+let resultado = (preco < 40) ? "Livro barato" : "Livro não é barato";
+console.log(resultado);
+
+let meuObjeto = {
+  propriedade1 : 'um texto',
+  propriedade2 : 4002
+}
+
+var avore = {
+  carvalhoI : 15
+}
