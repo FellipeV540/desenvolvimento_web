c01aa7faa195b68b9dcb1bce2a46a4ebf81e0b53
FELLIPE MARTINS VALLADARES <202401000991@alunos.ibmec.edu.br>
2024-03-25 11:01:46 -0300

    ex em sala concluido

---
+++ aula5/js1/js_script.js
@@ -0,0 +1,10 @@
+console.log('começou')
+
+let lista = [1, 2, 3, 4]
+
+console.log('no meio')
+
+lista.push('novo')
+
+console.log(lista.length)
+console.log('terminou')
--- aula5/lista.js
+++ aula5/lista.js
@@ -1,1 +1,18 @@
+let lista = [1, 2, 3, 4]
+console.log(lista)
+
+lista.push('5')
+console.log(lista)
+
+lista.pop()
+console.log(lista)
+
+const lista1 = [1, 2, 3]
+const ele1 = lista1.shift()
+
+console.log(lista1)
+console.log(ele1)
+
+lista1.unshift(0, 1)
+console.log(lista1)
 
